1a8dbafd23e82d2b64b04f70bbe65c68d430c56e
emprende <135877376+rutox09@users.noreply.github.com>
2026-05-18 20:31:18 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,21 +2,19 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '..')));
 app.use(express.json());
 
-// Votaciones guardadas en memoria
 let votaciones = [];
 
-// Obtener votaciones
 app.get('/api/votaciones', (req, res) => {
     res.json(votaciones);
 });
 
-// Crear votación
 app.post('/api/votaciones', (req, res) => {
+
     const { titulo, opciones } = req.body;
 
     const nueva = {
@@ -33,18 +31,20 @@ app.post('/api/votaciones', (req, res) => {
     res.json(nueva);
 });
 
-// Votar
 app.post('/api/votar/:id/:opcion', (req, res) => {
+
     const id = Number(req.params.id);
-    const opcionIndex = Number(req.params.opcion);
+    const opcion = Number(req.params.opcion);
 
     const votacion = votaciones.find(v => v.id === id);
 
     if (!votacion) {
-        return res.status(404).json({ error: 'No encontrada' });
+        return res.status(404).json({
+            error: 'No encontrada'
+        });
     }
 
-    votacion.opciones[opcionIndex].votos++;
+    votacion.opciones[opcion].votos++;
 
     res.json(votacion);
 });
